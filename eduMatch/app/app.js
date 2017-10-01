73863c9dcb65a0bbc4bec14834eca9b2fbe9de2a
Kazexiii <sn.owl@hotmail.com>
2017-10-01 09:07:18 -0700

    sef
    fese

--- eduMatch/app/app.js
+++ eduMatch/app/app.js
@@ -1,38 +1,84 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Alert, StyleSheet, Button, Text, View } from 'react-native';
 import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  StackNavigator,
+} from 'react-navigation';
+import Login from './views/Login'
+import Profile from './views/Profile'
+import Signup from './views/Signup'
 
-import Login from './views/Login.js';
-import secured from './views/Secured.js';
+const App = StackNavigator({
+  Login: { screen: Login },
+  Profile: { screen: Profile },
+  Signup: { screen: Signup },
+});
 
-export default class app extends Component {
-  state = {
-    isLoggedIn: false
-  }
+export default class app extends React.Component {
   render() {
+    return <App />
+    // return (
+    //   <View >
+    //     <NavigatorIOS
+    //       initialRoute={{
+    //         title: 'Welcome Student',
+    //         passProps: {index: 1},
+    //       }}
+    //       style={{flex: 1}}
+    //     />
+    //     <Button style = {styles.buttons} onPress={() => Alert.alert('login')} title="Login"/>
+    //   </View>
+    //
+    // )
+  }
+}
 
-    }
+class mentor extends React.Component {
+  static propTypes = {
+    route: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    navigator: PropTypes.object.isRequired,
+  }
+
+  constructor(props, context) {
+    super(props, context);
+    this._onForward = this._onForward.bind(this);
+  }
+
+  _onForward() {
+    let nextIndex = ++this.props.index;
+    this.props.navigator.push({
+      component: mentor,
+      title: 'Welcome Mentor',
+      passProps: {index: nextIndex}
+    });
+  }
+
+  render() {
+    return (
+      <View style = {styles.mentorButton}>
+        <Text>Current Scene: { this.props.title }</Text>
+        <Button
+          onPress={this._onForward}
+          title="Tap me to load the next scene"
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  mentorButton: {
+    position: 'relative',
+    top: 50,
+
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  buttons: {
+    position: 'relative',
+    top: 250,
+
+
+
+  }
 });
